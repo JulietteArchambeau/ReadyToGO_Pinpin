fddf9cc755a6597d0c55e70738320ebe803198a0
Juliette Archambeau <juli.archambeau@orange.fr>
2023-07-28 16:05:47 +0100

    predict GO with GF and GDM

---
+++ scripts/functions/make_eucli_plot.R
@@ -0,0 +1,25 @@
+# Function to plot the relationship between the Euclidean distance and the genomic offset
+
+make_eucli_plot <- function(X,Y,
+                            colors, color_names, 
+                            plot_title=NULL, 
+                            xlim=NULL,ylim=NULL,
+                            cex=1.2,pch=19,
+                            cex_legend=0.8,
+                            ylab,xlab ="Euclidean distance",
+                            legend_position="topleft",
+                            inset=c(0,0)) {
+  plot(X, Y, 
+       xlab = xlab,  ylab =ylab, 
+       cex = cex , pch = pch, 
+       col = colors,
+       xlim = xlim, ylim = ylim,
+       main = plot_title)
+  legend(legend_position, 
+         legend=unique(color_names),
+         col=unique(colors),
+         inset=inset,
+         bty="n", pch=pch, cex=cex_legend, ncol=1,
+         title="Main gene pools")
+  recordPlot()
+}
---
+++ scripts/functions/make_go_map.R
@@ -0,0 +1,32 @@
+library(rnaturalearth)
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+make_go_map <- function(dfcoord, 
+                        snp_set, 
+                        point_size=2, 
+                        x_limits = c(-10, 13),
+                        y_limits = c(31, 49),
+                        legend_position = "right",
+                        legend_box_background = "white",# "gray80"
+                        go_limits=NULL,
+                        ggtitle=NULL,
+                        gcm){
+  
+
+    point_go <- dfcoord %>% mutate(GO=go[[gcm]])  
+    
+  
+  p <-  ggplot() + 
+    geom_sf(data = world, fill="gray98") + 
+    theme_bw() +
+    scale_x_continuous(limits = x_limits) +
+    scale_y_continuous(limits = y_limits) + 
+    geom_point(data=point_go, aes(x=longitude,y=latitude,color=GO), size=point_size) + 
+    xlab("") + ylab("") +
+    ggtitle(gcm) +
+    theme(legend.position = legend_position,
+          legend.box.background = element_rect(colour = legend_box_background))  +
+    scale_color_gradientn(name = "Genomic offset", colours = rev(rainbow(5)), limits=go_limits)
+  
+  return(p)
+}
